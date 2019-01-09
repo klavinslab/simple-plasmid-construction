5cf856dd3e99dc84107d5df5c001bbf462178b27
Ben Keller <bjkeller@vt.edu>
2019-01-09 14:36:02 -0800

    Aquadoc initial commit

---
+++ js/definitions.js
@@ -0,0 +1,309 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "UW BIOFAB Simple Plasmid Construction",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/klavinslab/simple-plasmid-construction/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+            {
+              name: 'Assemble Plasmid',
+              path: 'operation_types/Assemble_Plasmid' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Check Plate',
+              path: 'operation_types/Check_Plate' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Clean Up Sequencing',
+              path: 'operation_types/Clean_Up_Sequencing' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Extract Gel Slice',
+              path: 'operation_types/Extract_Gel_Slice' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Make Miniprep',
+              path: 'operation_types/Make_Miniprep' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Make Overnight Suspension',
+              path: 'operation_types/Make_Overnight_Suspension' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Make PCR Fragment',
+              path: 'operation_types/Make_PCR_Fragment' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Plate Transformed Cells',
+              path: 'operation_types/Plate_Transformed_Cells' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Pour Gel',
+              path: 'operation_types/Pour_Gel' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Purify Gel Slice',
+              path: 'operation_types/Purify_Gel_Slice' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Run Gel',
+              path: 'operation_types/Run_Gel' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Send to Sequencing',
+              path: 'operation_types/Send_to_Sequencing' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Transform Cells',
+              path: 'operation_types/Transform_Cells' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Upload Sequencing Results',
+              path: 'operation_types/Upload_Sequencing_Results' + '.md',
+              type: "local-md"
+            },
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+            {
+              name: 'DNA Library',
+              path: 'sample_types/DNA_Library'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'E coli strain',
+              path: 'sample_types/E_coli_strain'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Fragment',
+              path: 'sample_types/Fragment'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Oligo Pool',
+              path: 'sample_types/Oligo_Pool'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Plasmid',
+              path: 'sample_types/Plasmid'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Primer',
+              path: 'sample_types/Primer'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Yeast Strain',
+              path: 'sample_types/Yeast_Strain'  + '.md',
+              type: "local-md"
+            },
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+            {
+              name: '1 ng/µL Fragment Stock',
+              path: 'object_types/1_ng_L_Fragment_Stock'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: '1 ng/µL Plasmid Stock',
+              path: 'object_types/1_ng_L_Plasmid_Stock'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: '50 mL 0.8 Percent Agarose Gel in Gel Box',
+              path: 'object_types/50_mL_0.8_Percent_Agarose_Gel_in_Gel_Box'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: '50X PCR Template',
+              path: 'object_types/50X_PCR_Template'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Checked E coli Plate of Plasmid',
+              path: 'object_types/Checked_E_coli_Plate_of_Plasmid'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'DNA Mix',
+              path: 'object_types/DNA_Mix'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'E coli Plate of Plasmid',
+              path: 'object_types/E_coli_Plate_of_Plasmid'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'E. coli Comp Cell Batch',
+              path: 'object_types/E._coli_Comp_Cell_Batch'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Fragment Stock',
+              path: 'object_types/Fragment_Stock'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Gel Slice',
+              path: 'object_types/Gel_Slice'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Gibson Reaction Result',
+              path: 'object_types/Gibson_Reaction_Result'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Ligation product',
+              path: 'object_types/Ligation_product'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Plasmid Glycerol Stock',
+              path: 'object_types/Plasmid_Glycerol_Stock'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Plasmid Stock',
+              path: 'object_types/Plasmid_Stock'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Primer Aliquot',
+              path: 'object_types/Primer_Aliquot'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Sequencing Stripwell',
+              path: 'object_types/Sequencing_Stripwell'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Stripwell',
+              path: 'object_types/Stripwell'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Stripwell of Digested Plasmid',
+              path: 'object_types/Stripwell_of_Digested_Plasmid'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'TB Overnight of Plasmid',
+              path: 'object_types/TB_Overnight_of_Plasmid'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Transformed E. coli Aliquot',
+              path: 'object_types/Transformed_E._coli_Aliquot'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Unverified PCR Fragment',
+              path: 'object_types/Unverified_PCR_Fragment'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Unverified Plasmid Stock',
+              path: 'object_types/Unverified_Plasmid_Stock'  + '.md',
+              type: "local-md"
+            },
+          
+        ]
+      }
+    
+
+  ]
+
+};
